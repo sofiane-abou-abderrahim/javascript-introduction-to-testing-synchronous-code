9fb2b255e72fc1f1bbb549a82be3d483e5aa629f
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-30 14:14:01 +0200

    deleted test 2 and multiple expects

--- util.test.js
+++ util.test.js
@@ -3,11 +3,4 @@ const { generateText } = require('./util');
 test('should output name and age', () => {
   const text = generateText('Max', 29);
   expect(text).toBe('Max (29 years old)');
-  const text2 = generateText('Anna', 28);
-  expect(text2).toBe('Anna (28 years old)');
-});
-
-test('should output data-less text', () => {
-  const text = generateText('', null);
-  expect(text).toBe(' (null years old)');
 });
