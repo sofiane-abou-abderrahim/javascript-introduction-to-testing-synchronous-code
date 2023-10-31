de85b6aeb3f01f5150a53291488c8d218f8bd421
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-31 18:33:19 +0200

    written test

--- util.test.js
+++ util.test.js
@@ -10,3 +10,13 @@ test('should generate a valid text output', () => {
   const text = checkAndGenerate('Max', 29);
   expect(text).toBe('Max (29 years old)');
 });
+
+test('should click around', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=1920,1080']
+  });
+  const page = await browser.newPage();
+  await page.goto('http://127.0.0.1:5500/index.html');
+});
